ada173c1220b0cb0b14e3f9bd13d395a23f88fd5
Alan Caldelas <ajcaldelas@gmail.com>
2021-10-04 21:48:25 -0500

    Added Terraform offline

---
+++ terraform/test/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/terraform-provider-vsphere/vsphere" {
+  version     = "2.0.2"
+  constraints = "2.0.2"
+  hashes = [
+    "h1:TIERO1/jkMNiH+HlU3IDa4KUTV9PyOW8/PmzOL4agAQ=",
+  ]
+}
---
+++ terraform/test/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "terraform-provider-vsphere/vsphere"
+      version = "2.0.2"
+    }
+  }
+  required_version  = ">= 0.13"
+}
+provider "vsphere" {
+  user = "administrator@vsphere.local"
+  password = ""
+  vsphere_server = "10.0.0.82"
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+    name = "Datacenter"
+}
+
+data "vsphere_datastore" "datastore" {
+    name = "vsanDatastore"
+    datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+    name = "New Cluster"
+    datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "iso_datastore" {
+    name = "vsanDatastore"
+    datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+    name = "New Resource Pool"
+    datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+    name = "DSwitch-VM Network"
+    datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+    name = "Centos8"
+    datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "terraform-test"
+        domain    = "test.internal"
+      }
+
+      network_interface {}
+      
+    }
+  }
+}
